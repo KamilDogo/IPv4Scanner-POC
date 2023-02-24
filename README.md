0c37e445fa816161ddd1e77714da65bb3c7ce676
KamilDogo <125549765+KamilDogo@users.noreply.github.com>
2023-02-24 15:37:38 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+﻿
+# IPv4Scanner-POC
+**Pv4Scanner-POC is a program that performs a port scan of the entered IP address and automatically downloads a PoC code associated with the vulnerability if a CVE vulnerability exists.**
+
+It was created utilizing the API of motikan2010's [PoC-in-GitHub]([https://poc-in-github.motikan2010.net/](https://poc-in-github.motikan2010.net/)) and the search engine of [Criminal IP]([https://www.criminalip.io](https://www.criminalip.io/)).
+
+You need to create an API key, accessible through a free account at  [criminalip.io](http://criminalip.io/).
+  
+# Installation  
+  
+Clone repository:  
+  
+```  
+$ git clone https://github.com/KamilDogo/IPv4Scanner-POC.git  
+```  
+  
+```  
+$ cd ipv4scanner-poc
+```  
+  
+```  
+$ python3 -m venv .venv  
+$ source .venv/bin/activate  
+```  
+  
+```  
+$ pip3 install -r requirements.txt  
+```  
+  
+  
+  
+# Usage  
+  
+```  
+$ python3 ipv4scanner-poc.py --api_key [your-criminalip-api-key]  
+```  
+  
+  
+  
+# Optional Arguments  
+  
+| Flag | MetaVar | Usage |  
+| -------------- | ----------- | ----------------------------------------------------- |  
+| `--api_key` | **API key** | api authentication with a valid [criminalip.io](http://criminalip.io) api key |  
+| `--ip` | **Exploit Query** | Return IP data from query and download cve github source code |
---
+++ ipv4scanner-poc.py
@@ -0,0 +1,104 @@
+import argparse 
+import os
+import requests   
+import colorama 
+import logging
+import sys
+   
+
+def debug(msg):
+    globals()['logger'].debug(str(msg))
+
+class Cip_Github_Downloader() : 
+
+    def __init__(self, api_key = None, ip = None, path = None) : 
+        
+        self._api_key = api_key
+        self._ip = ip
+        self._session = requests.Session()
+        self._base_url =  'https://api.criminalip.io'  
+        self._path = path 
+       
+        
+    def run(self):
+        
+        if self._api_key and self._ip :   
+          
+            base_url =  "{}{}".format(self._base_url , '/v1/ip/data') 
+            params = {'ip': self._ip}
+            headers = {'x-api-key': self._api_key} 
+            result = self._session.get(base_url , params=params, headers=headers)  
+ 
+            if result.status_code == 200:
+               result = result.json()
+               self.download(result)
+            else:
+                result = None  
+ 
+
+    def download(self, res = None ):
+        if not os.path.exists('{}/download'.format(self._path)):
+            os.mkdir('{}/download'.format(self._path)) 
+        
+        for vd in res['vulnerability']['data']:
+            if vd['cve_id']:
+                vuln = vd['cve_id'] 
+                github_url = motikan_api(vuln)
+                if github_url is not None : 
+                    output = os.system('cd {}/download && git clone {}'.format(self._path, github_url))
+                    if int(output) == 0:
+                        print("download success into 'download' folder")  
+                    elif int(output) == 32768:
+                        print("already exists in 'download' folder") 
+                else:
+                    print("no data")
+
+
+                break  
+
+
+def motikan_api(cve_id):
+    url = 'https://poc-in-github.motikan2010.net/api/v1'  
+    params = {'cve_id': cve_id} 
+    result = requests.get(url=url, params=params)
+    github_url = None 
+
+    if result.status_code == 200:
+        result = result.json() 
+        if result['pocs']:
+            github_url = result['pocs'][0]['html_url'] 
+
+    return github_url       
+   
+ 
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Find database by using criminalip service')
+    parser.add_argument('--api_key', help='criminalip api key')
+    parser.add_argument('--ip', help='scan an IP') 
+    args = parser.parse_args()
+    path = os.getcwd()  
+ 
+    if args.api_key:
+        api_key = args.api_key  
+
+        if os.path.isfile('{}/.api_key'.format(path)):
+            x = input('Do you want to update your api_key? (Y/n) ')
+            if x in ['y', 'Y']:
+                with open('{}/.api_key'.format(path), 'w') as file:
+                    file.write(api_key)
+                print('\nSuccessfully updated\n')
+            else:
+                print('\nCanceled\n')
+        else:
+            with open('{}/.api_key'.format(path), 'w') as file:
+                file.write(api_key)
+    else:
+        with open('{}/.api_key'.format(path), 'r') as file:
+            api_key = file.readline().strip()
+            
+
+    downloader = Cip_Github_Downloader(args.api_key, args.ip , path)    
+    downloader.run()
+
+   
